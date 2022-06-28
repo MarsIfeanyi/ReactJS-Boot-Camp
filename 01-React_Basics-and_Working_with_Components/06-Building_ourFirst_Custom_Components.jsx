bdcae3a4b5f15aab6fbf14f35002d20217511a3e
MarsIfeanyi <marschalice@gmail.com>
2022-06-28 15:31:40 +0000

    Building First custom component

---
+++ 01-React_Basics-and_Working_with_Components/06-Building_ourFirst_Custom_Components.jsx
@@ -0,0 +1,16 @@
+/**
+ * How is a component written in React?
+ * Hint: Component is written just a as function.
+ * A component in React is just a JS function
+ *
+ * Hint: In naming your component the convention is to repeat the same file name without the extension ie the function component name should be the same as the file name
+ *
+ * Hint: The key difference between the custom HTML (Component) to the built in HTML elements is that is not starting with a lowercase character, but with an uppercase character...You custom HTML must start with an uppercase, so that React is able to detect that it is a custom component
+ *
+ * Hint: The simple rule that react applies is that: Lowercase elements are built in HTML elements while uppercase elements are elements defined by the developer of the app.
+ *
+ * Hint: In Summary the steps you building components are:
+ * 1. Create the component using JS function, returning some HTML codes
+ * 2. Export the created component
+ * 3. IMport the component in the file where you want to use it so that they are in the JSX code, and you can use it like an HTML element,starting with an uppercase character
+ */
