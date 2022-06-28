3d83dc042d9595e4a4924baacd89940351e34025
MarsIfeanyi <marschalice@gmail.com>
2022-06-28 15:36:27 +0000

    Write ExpenseItem Component

---
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -0,0 +1,9 @@
+// This hold the expense item components
+
+//Creating a component
+function ExpenseItem() {
+  return <h2>Expense item!</h2>;
+} //Here we return the HTML code, the JSX code which should be brought onto the screen by the component
+
+// To use the component we have to export it
+export default ExpenseItem;
