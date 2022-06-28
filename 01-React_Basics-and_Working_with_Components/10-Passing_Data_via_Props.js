b22e9d7fcb3ca35bc6f5d283a8fd429ad47f6a7e
MarsIfeanyi <marschalice@gmail.com>
2022-06-28 19:50:53 +0000

    Passing Data via Props

---
+++ 01-React_Basics-and_Working_with_Components/10-Passing_Data_via_Props.js
@@ -0,0 +1,25 @@
+/**
+ * We can make our code reusable by using parameters and a concept called props in React
+ *
+ * We can pass data to custom components by adding an attribute and inside of that component we can then get access to all the attributes, which might have been set on our custom component.
+ *
+ * Hint: We are just building our own custom HTML and again just as HTML has attributes, it turns out that with React, our own custom components can also have attributes.
+ *
+ * Props = Properties...we can set properties of our own custom components
+ *
+ * Hint: We add the attributes to the custom HTML we created in App.js
+ *
+ * Hint: React will ensure that we get one parameter in every component which we use as a component and that one parameter will be an object which holds all the received attributes as properties, hence the name 'Props'.
+ * The props will get access to all the attributes we defined in our custom element (ie the JSX code in the App.js).
+ *
+ * The props have key-value pairs as well. The keys are the attributes name defined in the custom HTML inside the App.js file, and the values is the values set in the {}
+ *
+ * Hint: The key you access from your props object has to be the same as the name you used for attribute.
+ *
+ * Hint:Choose/use attribute names which are self explanatory and makes sense.
+ *
+ * Hint: You can make your components truly reusable and configurable by using the props concept. Props is one of the key concept react has to offer.
+ *
+ *
+ * Hint: You define your attribute in the root component file ie App.js and the use props to call it inside the custom component where you want to use it
+ */
