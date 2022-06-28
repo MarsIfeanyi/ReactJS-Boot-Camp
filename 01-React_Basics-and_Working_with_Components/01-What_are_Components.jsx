a48765ee5a1b12efdf5469059df88251f71f057a
MarsIfeanyi <marschalice@gmail.com>
2022-06-28 13:56:18 +0000

    React Basics and working with Components

---
+++ 01-React_Basics-and_Working_with_Components/01-What_are_Components.jsx
@@ -0,0 +1,17 @@
+/**
+ * React is a JS library for building user interfaces. HTML and CSS are used to build user interface, however React makes building complex interactive and reactive user interfaces simpler.
+ *
+ * Hint: React is all about Component, because all UIs are made up of components.
+ *
+ * Components are reusable building blocks in your user interface. Components is a combination of HTML codes, Css for styling and possibly JS for some logic.
+ *
+ * Why Components?
+ * 1. Reusability: Helps us to avoid repetition
+ * 2. Separation of Concerns: Helps us to keep our code base small and manageable.
+ *
+ * React allows you to create re-usable and reactive components consisting of HTML and JS(and CSS).
+ * React uses a declarative approach for building components ie you define the desired target state(s) and let React figure out the actual JS DOM instructions
+ *
+ *
+ *
+ */
---
+++ 01-React_Basics-and_Working_with_Components/02-Creating_a_new_React_Project.jsx
@@ -0,0 +1,7 @@
+/**
+ * https://reactjs.org/docs/create-a-new-react-app.html
+ *
+ * The package.json file holds all the dependencies of the project
+ * The Src folder holds the actual source code you will be working on
+ *
+ */
