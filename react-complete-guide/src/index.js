0afeab18fec1c5dddcccc634c86c830880d69044
MarsIfeanyi <marschalice@gmail.com>
2022-06-28 15:34:50 +0000

    Add some Documentation

--- react-complete-guide/src/index.js
+++ react-complete-guide/src/index.js
@@ -1,7 +1,8 @@
-import ReactDOM from 'react-dom/client';
+import ReactDOM from "react-dom/client"; //Here we are importing ReactDOM object from the react-dom 3rd party library, which is one of the dependencies that is downloaded and installed locally
 
-import './index.css';
-import App from './App';
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root")); //Here CreateRoot, creates the main entry point, the main hook of the overall user interface you are about to build with React and store it in a variable called 'root'
+
 root.render(<App />);
