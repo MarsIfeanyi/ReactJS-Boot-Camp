c3494eb1541202a22335176bf3c64d0997766b44
MarsIfeanyi <marschalice@gmail.com>
2022-06-28 15:31:05 +0000

    How React Works

---
+++ 01-React_Basics-and_Working_with_Components/05-How_React_Works.jsx
@@ -0,0 +1,11 @@
+/**
+ * We build our own custom HTML elements with React.
+ *
+ * Component is basically just a custom HTML element, which we create using the Declarative approach ie with react we defined the desired target state and then react is responsible for generating and running the actual DOM instructions which updates whats visible on the screen
+ *
+ *
+ *
+ *
+ *
+ *
+ */
