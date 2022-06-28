d11427744d31d58eb4f65ad6f343f477c22a5a55
MarsIfeanyi <marschalice@gmail.com>
2022-06-28 15:34:05 +0000

    Import ExpenseItem custom component

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,9 +1,21 @@
+import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
+  //Creating the paragraph element using regular JS
+
+  // const para = document.createElement("p");
+  // para.textContent = "Front-End Engineer";
+  // document.getElementById("root").append(para);
+
   return (
     <div>
       <h2>Let's get started!</h2>
-    </div>
+      <p>Front-End Engineer</p>
+
+      <ExpenseItem></ExpenseItem>
+    </div> //Desired target state
   );
+  //This is JSX and not pure html...JSX is HTML code inside of JS
 }
 
 export default App;
