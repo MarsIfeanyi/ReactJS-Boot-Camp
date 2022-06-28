980070efc7713d54bdd041e632d3a8da35b27dff
MarsIfeanyi <marschalice@gmail.com>
2022-06-28 18:02:51 +0000

    Passing Data via Props

--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -1,11 +1,7 @@
 import "./ExpenseItem.css";
 
 //Creating a component
-function ExpenseItem() {
-  const expenseDate = new Date(2022, 5, 20);
-  const expenseTitle = "Car Insurance";
-  const expenseAmount = 294.67;
-
+function ExpenseItem(props) {
   //   return (
 
   // Hardcoded Data
@@ -22,10 +18,10 @@ function ExpenseItem() {
   //Dynamic Data
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">${expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
