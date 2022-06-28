569bf36ac50d668d91463d716e0edf9394c2e2df
MarsIfeanyi <marschalice@gmail.com>
2022-06-28 19:03:42 +0000

    Split ExpenseDate to its own custom component

--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -1,4 +1,5 @@
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 
 //Creating a component
 function ExpenseItem(props) {
@@ -17,17 +18,9 @@ function ExpenseItem(props) {
 
   //Dynamic Data
 
-  const month = props.date.toLocaleString("en-US", { month: "long" });
-  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
-  const year = props.date.getFullYear();
-
   return (
     <div className="expense-item">
-      <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
-      </div>
+      <ExpenseDate date={props.date}></ExpenseDate>
 
       <div className="expense-item__description">
         <h2>{props.title}</h2>
