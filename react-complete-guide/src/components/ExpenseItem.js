3fe0778c1f779431e13534068d6ec943310e2092
MarsIfeanyi <marschalice@gmail.com>
2022-06-28 17:16:59 +0000

    Dynamic update data

--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -1,15 +1,31 @@
 import "./ExpenseItem.css";
 
-// This hold the expense item components
-
 //Creating a component
 function ExpenseItem() {
+  const expenseDate = new Date(2022, 5, 20);
+  const expenseTitle = "Car Insurance";
+  const expenseAmount = 294.67;
+
+  //   return (
+
+  // Hardcoded Data
+
+  //     <div className="expense-item">
+  //       <div>June 20th 2022</div>
+  //       <div className="expense-item__description">
+  //         <h2>Car Insurance</h2>
+  //         <div className="expense-item__price">$294.67</div>
+  //       </div>
+  //     </div>
+  //   );
+
+  //Dynamic Data
   return (
     <div className="expense-item">
-      <div>June 20th 2022</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$294.67</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
       </div>
     </div>
   );
