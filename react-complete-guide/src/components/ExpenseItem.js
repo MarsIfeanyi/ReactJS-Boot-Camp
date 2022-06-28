d09ccd69ac3eeb96da27b5ac697b474bbd5f45ba
MarsIfeanyi <marschalice@gmail.com>
2022-06-28 16:05:54 +0000

    Write more complex JSX code

--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -2,7 +2,15 @@
 
 //Creating a component
 function ExpenseItem() {
-  return <h2>Expense item!</h2>;
+  return (
+    <div>
+      <div>June 20th 2022</div>
+      <div>
+        <h2>Car Insurance</h2>
+        <div>$294.67</div>
+      </div>
+    </div>
+  );
 } //Here we return the HTML code, the JSX code which should be brought onto the screen by the component
 
 // To use the component we have to export it
