6a2d9450560948fb1a39b87c1b51876bf3c5d3e2
MarsIfeanyi <marschalice@gmail.com>
2022-06-28 15:30:41 +0000

    Analyzing a standard React Project

---
+++ 01-React_Basics-and_Working_with_Components/03-Analyzing_a_standard_React_project.jsx
@@ -0,0 +1,12 @@
+/**
+ * The index.js file holds the code which is executed initially
+ *
+ * CreateRoot, creates the main entry point, the main hook of the overall user interface you are about to build with React
+ *
+ * The App.js is the root ie the main custom component and we might not put it inside the components folder... The App.js is rendered directly into the single HTML page with the help of React Dom render instruction
+ *
+ * Hint: The convention for naming files in react is every new words starts with capital letter
+ *
+ *
+ *
+ */
