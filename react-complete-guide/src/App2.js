b2e49a386ed0d104a6cb04abd8fb95df3ab1fdfa
MarsIfeanyi <marschalice@gmail.com>
2022-06-29 05:49:45 +0000

    Update file

---
+++ react-complete-guide/src/App2.js
@@ -0,0 +1,55 @@
+// import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses/Expenses";
+
+function App() {
+  //Creating the paragraph element using regular JS
+
+  // const para = document.createElement("p");
+  // para.textContent = "Front-End Engineer";
+  // document.getElementById("root").append(para);
+
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
+
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      {/* <p>Front-End Engineer</p> */}
+
+      <Expenses item={expenses}></Expenses>
+    </div> //Desired target state
+  );
+  //This is JSX and not pure html...JSX is HTML code inside of JS
+}
+
+export default App;
+
+/**
+ * How React works with the above JSX code under the hood
+
+return React.createElement(
+  "div",
+  {},
+  React.createElement("h2", {}, "Let's get started!"),
+  React.createElement(Expenses, { items: expenses })
+);
+ */
