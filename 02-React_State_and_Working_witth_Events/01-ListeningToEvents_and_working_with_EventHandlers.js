bba86dfe3ff0808e4bfbf6755c52cf83c5657800
MarsIfeanyi <marschalice@gmail.com>
2022-06-29 09:38:08 +0000

    Listening to Events and working with Event Handlers

---
+++ 02-React_State_and_Working_witth_Events/01-ListeningToEvents_and_working_with_EventHandlers.js
@@ -0,0 +1,6 @@
+/**
+ * In react we add an event listener by going to the JSX element like the 'button' and there we add as special prop... Prop with starts with 'on'.
+ *
+ * Hint: React expose all the default event which starts with 'on' as props.
+ * The value assigned to the special prop is a function which contains code that should be executed when certain operation happens and we just point at the function inside {} ie you don't add () after the function name
+ */
