c41d28d6d1e9fee730193fb3992154ed1460bfb2
MarsIfeanyi <marschalice@gmail.com>
2022-06-29 05:47:26 +0000

    Composition children props

---
+++ 01-React_Basics-and_Working_with_Components/11-Composition__Children_Props.js
@@ -0,0 +1,15 @@
+/**
+ * Composition is building user interface from smaller building blocks.
+ *
+ * In web dev, Card simply means some kind of container look with rounded corners, drop shadows.
+ *
+ * Hint: Components are the custom HTML elements where you combine HTML code(JSX code)  and styling
+ *
+ *
+ * Hint: You can't just your custom components like that as wrappers for other components ie having content between the opening and closing tags of that component doesn't just work like that.
+ *
+ * Props children is a special props which is built into React and every components has it even if you didn't set it explicitly.
+ * Hint: Children is a reserved name and the value of the children props will always be the content content between the opening and closing tags of your custom component
+ *
+ * Hint: Whenever you combine components, you are using composition. Composition is very important when working with react and an important part of composition is the 'props.children' feature which allows you to also create wrapper components
+ */
