47260f6831446eec3d85cbc2d9d3a3d94bf44f35
MarsIfeanyi <marschalice@gmail.com>
2022-06-29 08:57:35 +0000

    Add NewExpense component

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,4 +1,5 @@
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 //Using arrow function
 const App = () => {
@@ -26,7 +27,7 @@ const App = () => {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense></NewExpense>
 
       <Expenses item={expenses}></Expenses>
     </div>
