9ac79d1b1640383a84566f20b7579d72a1ce319f
MarsIfeanyi <marschalice@gmail.com>
2022-06-30 23:53:08 +0000

    Child to Parent Component communication

--- react-complete-guide/src/components/NewExpense/NewExpense.js
+++ react-complete-guide/src/components/NewExpense/NewExpense.js
@@ -2,10 +2,20 @@ import React from "react";
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
-const NewExpense = () => {
+const NewExpense = (props) => {
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString(),
+    }; // ...enteredExpenseData,Is the object generated in the submit Handler in the 'ExpenseForm'. we use the spread operator to pull out all the key values pairs and add it into this new object
+
+    // console.log(expenseData);
+    props.onAddExpense(expenseData);
+  };
+
   return (
     <div className="new-expense">
-      <ExpenseForm></ExpenseForm>
+      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}></ExpenseForm>
     </div>
   );
 };
