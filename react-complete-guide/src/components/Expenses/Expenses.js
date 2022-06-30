330017af7fc4c5111e3739a6fd9c5185b43972d4
MarsIfeanyi <marschalice@gmail.com>
2022-06-30 23:55:55 +0000

    Update file

--- react-complete-guide/src/components/Expenses/Expenses.js
+++ react-complete-guide/src/components/Expenses/Expenses.js
@@ -6,27 +6,25 @@ function Expenses(props) {
   return (
     <Card className="expenses">
       <ExpenseItem
-        title={props.item[0].title}
-        date={props.item[0].date}
-        amount={props.item[0].amount}
+        title={props.items[0].title}
+        date={props.items[0].date}
+        amount={props.items[0].amount}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={props.items[1].title}
+        date={props.items[1].date}
+        amount={props.items[1].amount}
       ></ExpenseItem>
 
       <ExpenseItem
-        title={props.item[1].title}
-        date={props.item[1].date}
-        amount={props.item[1].amount}
+        title={props.items[2].title}
+        date={props.items[2].date}
+        amount={props.items[2].amount}
       ></ExpenseItem>
-
       <ExpenseItem
-        title={props.item[2].title}
-        date={props.item[2].date}
-        amount={props.item[2].amount}
-      ></ExpenseItem>
-
-      <ExpenseItem
-        title={props.item[3].title}
-        date={props.item[3].date}
-        amount={props.item[3].amount}
+        title={props.items[3].title}
+        date={props.items[3].date}
+        amount={props.items[3].amount}
       ></ExpenseItem>
     </Card>
   );
