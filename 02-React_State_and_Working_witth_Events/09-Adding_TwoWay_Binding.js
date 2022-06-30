a1bfa0d62e650e5062faa4e25428d46a720e083e
MarsIfeanyi <marschalice@gmail.com>
2022-06-30 23:44:51 +0000

    Adding Two way Binding

---
+++ 02-React_State_and_Working_witth_Events/09-Adding_TwoWay_Binding.js
@@ -0,0 +1,114 @@
+/**
+ * Two way Binding means that for inputs we don't just listen to changes but we can also pass a new value back into the input, so that we can reset or change the input programmatically.
+ *
+ * Hint: We achieve this in the ExpenseForm by adding the 'value' attribute to the form, so that we change the state, we also change the input.
+ *
+ * Hint: Two way Binding is very important concept in React especially when you are working with forms because it allows you to gather user input and also change it upon form submission
+ */
+
+import React, { useState } from "react";
+
+import "./ExpenseForm.css";
+
+const ExpenseForm = () => {
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+
+  // const [userInput, setUserInput] = useState({
+  //   enteredTitle: "",
+  //   enteredAmount: "",
+  //   enteredDate: "",
+  // });
+
+  const titleChangeHandler = (event) => {
+    setEnteredTitle(event.target.value);
+
+    // setUserInput({
+    //   ...userInput,
+    //   enteredTitle: event.target.value,
+    // });
+
+    //Altenative way for updating state that depends on the previous state
+    // setUserInput((prevState) => {
+    //   return { ...prevState, enteredTitle: event.target.value };
+    // });
+  };
+
+  const amountChangeHandler = (event) => {
+    setEnteredAmount(event.target.value);
+
+    // setUserInput({
+    //   ...userInput,
+    //   enteredAmount: event.target.value,
+    // });
+  };
+
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
+
+    // setUserInput({
+    //   ...userInput,
+    //   enteredDate: event.target.value,
+    // });
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault(); //Prevents the submission of form on page reload.This is default behavior of JS
+
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    }; //Make sure that the property names exist and hold the correct value
+    console.log(expenseData);
+
+    //Two way Binding: Clearing the input fields
+    setEnteredTitle("");
+    setEnteredAmount("");
+    setEnteredDate("");
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label>Title</label>
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          ></input>
+        </div>
+
+        <div className="new-expense__control">
+          <label>Amount</label>
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            value={enteredAmount}
+            onChange={amountChangeHandler}
+          ></input>
+        </div>
+
+        <div className="new-expense__control">
+          <label>Date</label>
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2022-12-31"
+            value={enteredDate}
+            onChange={dateChangeHandler}
+          ></input>
+        </div>
+
+        <div className="new-expense__actions">
+          <button type="submit">Add Expense</button>
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default ExpenseForm;
