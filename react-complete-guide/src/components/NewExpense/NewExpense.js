1a3d0f171d40cf5910aa5799ea9b1577c378c28d
MarsIfeanyi <marschalice@gmail.com>
2022-07-01 00:32:26 +0000

    Update file

--- react-complete-guide/src/components/NewExpense/NewExpense.js
+++ react-complete-guide/src/components/NewExpense/NewExpense.js
@@ -7,10 +7,10 @@ const NewExpense = (props) => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString(),
-    }; // ...enteredExpenseData,Is the object generated in the submit Handler in the 'ExpenseForm'. we use the spread operator to pull out all the key values pairs and add it into this new object
+    }; // ...enteredExpenseData,Is the object generated in the submit Handler in the 'ExpenseForm'. we use the spread operator to pull out all the key values pairs and add it into this new object (expenseData)
 
     // console.log(expenseData);
-    props.onAddExpense(expenseData);
+    props.onAddExpense(expenseData); //Child to parent communication
   };
 
   return (
