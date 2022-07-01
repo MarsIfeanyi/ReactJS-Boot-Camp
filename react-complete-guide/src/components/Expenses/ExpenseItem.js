17e01b2b25e75c8bd059ca78ddc0392dc4dcb438
MarsIfeanyi <marschalice@gmail.com>
2022-07-01 02:29:14 +0000

    Listen to State change on ExpensesFilter component

--- react-complete-guide/src/components/Expenses/ExpenseItem.js
+++ react-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -1,29 +1,34 @@
-import React, { useState } from "react";
+import React from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
 
 //Creating a component, using arrow function
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title);
-  const clickHandler = () => {
-    setTitle("Updated!!!");
-    console.log(title);
-  };
+  // const [title, setTitle] = useState(props.title);
+  // const clickHandler = () => {
+  //   setTitle("Updated!!!");
+  //   console.log(title);
+  // };
   //Dynamic Data
 
   return (
-    <Card className="expense-item">
-      <ExpenseDate date={props.date}></ExpenseDate>
+    <div>
+      <ExpensesFilter></ExpensesFilter>
 
-      <div className="expense-item__description">
-        <h2>{title}</h2>
-        <div className="expense-item__price">${props.amount}</div>
-      </div>
+      <Card className="expense-item">
+        <ExpenseDate date={props.date}></ExpenseDate>
 
-      {/* Here we just point at the function and not call the function */}
-      <button onClick={clickHandler}>Change Title </button>
-    </Card> //Using Card components as a wrapper
+        <div className="expense-item__description">
+          <h2>{props.title}</h2>
+          <div className="expense-item__price">${props.amount}</div>
+        </div>
+
+        {/* Here we just point at the function and not call the function */}
+        {/* <button onClick={clickHandler}>Change Title </button> */}
+      </Card>
+    </div>
   );
 }; //Here we return the HTML code, the JSX code which should be brought onto the screen by the component
 
