c3520514ed0642abb2c524f2035235a134e30633
MarsIfeanyi <marschalice@gmail.com>
2022-07-01 02:34:42 +0000

    Lifting State Up

---
+++ 02-React_State_and_Working_witth_Events/11-Lifting_State_up.js
@@ -0,0 +1,7 @@
+/**
+ * Hint: We move data from child to parent component by utilizing props to receive a function from the parent component which we call in the child component.
+ *
+ * We don't have direct connection between two sibling components, instead we can only communicate from parent to child and from child to parent
+ *
+ * Lifting the State up is moving data from the child component to some parent component to either use it there or to then pass it down to some other child component
+ */
