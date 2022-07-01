a49b493c35e1ed7210cdaf8840e85fb81395ef1b
MarsIfeanyi <marschalice@gmail.com>
2022-07-01 00:01:11 +0000

    Move file to UI folder

--- react-complete-guide/src/components/Card.js
+++
@@ -1,9 +0,0 @@
-import "./Card.css";
-
-function Card(props) {
-  const classes = "card " + props.className; //Hint: There is white space after card before the closing quote. Here anything that is received as className from outside is added here. card is a class defined in the Card.css
-
-  return <div className={classes}>{props.children}</div>;
-}
-
-export default Card;
