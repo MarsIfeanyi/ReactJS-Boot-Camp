1e13a826db00d204e7ab8df99771756ef2e7be46
Mars Ifeanyi <Marschalice@gmail.com>
2022-07-10 23:55:43 +0100

    Add New Expense Button

--- react-complete-guide/src/components/NewExpense/NewExpense.js
+++ react-complete-guide/src/components/NewExpense/NewExpense.js
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
 const NewExpense = (props) => {
+  const [isEditing, setIsEditing] = useState(false);
+
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  };
+
+  const stopEditingHandler = () => {
+    setIsEditing(false);
+  };
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
@@ -11,11 +21,22 @@ const NewExpense = (props) => {
 
     // console.log(expenseData);
     props.onAddExpense(expenseData); //Child to parent communication
+    setIsEditing(false); //Closes the form after submission
   };
 
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}></ExpenseForm>
+      {/* We want to show the 'Add New Expense' Button if we are not editing ie if isEditing is false*/}
+      {!isEditing && (
+        <button onClick={startEditingHandler}> Add New Expense </button>
+      )}
+      {/* Here we want to show the form if isEditing is true */}
+      {isEditing && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={stopEditingHandler}
+        ></ExpenseForm>
+      )}
     </div>
   );
 };
