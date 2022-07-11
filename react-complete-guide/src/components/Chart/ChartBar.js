573e47c1d1a45e962d6e10397220a54057f29bb8
Mars Ifeanyi <Marschalice@gmail.com>
2022-07-11 02:20:55 +0100

    Adding Dynamic styles

--- react-complete-guide/src/components/Chart/ChartBar.js
+++ react-complete-guide/src/components/Chart/ChartBar.js
@@ -1,3 +1,22 @@
-const ChartBar = (props) => {};
+const ChartBar = (props) => {
+  let barFillHeight = "0%";
+
+  if (props.max > 0) {
+    barFillHeight = Math.round((props.value / props.maxValue) * 100) + "%";
+  }
+  return (
+    <div className="chart-bar">
+      <div className="chart-bar__inner">
+        <div
+          className="chart-bar__fill"
+          style={{ height: barFillHeight }}
+        ></div>
+      </div>
+      <div className="chart-bar__label">{props.label}</div>
+    </div>
+  );
+};
 
 export default ChartBar;
+
+//Hint: Style uses JS object as a value. if the css property has a dash in its name then put it in quote as a string or else use camel case ie background-color = 'background-color' or backgroundColor
