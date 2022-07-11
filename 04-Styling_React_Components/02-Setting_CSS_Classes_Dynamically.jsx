e565ff0b179d0dc9f714d8e510958c7f765760c5
Mars Ifeanyi <Marschalice@gmail.com>
2022-07-11 16:51:11 +0100

    Setting CSS Classes Dynamically

---
+++ 04-Styling_React_Components/02-Setting_CSS_Classes_Dynamically.jsx
@@ -0,0 +1,84 @@
+import React, { useState } from "react";
+
+import Button from "../../UI/Button/Button";
+import "./CourseInput.css";
+
+const CourseInput = (props) => {
+  const [enteredValue, setEnteredValue] = useState("");
+
+  const [isValid, setIsValid] = useState(true);
+
+  const goalInputChangeHandler = (event) => {
+    if (event.target.value.trim().length > 0) {
+      setIsValid(true);
+    }
+
+    setEnteredValue(event.target.value);
+  };
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+
+    //trim is a built method that we use to remove white spaces
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
+    props.onAddGoal(enteredValue);
+  };
+
+  return (
+    <form onSubmit={formSubmitHandler}>
+      {/* Setting CSS Classes Dynamically */}
+      <div className={`form-control ${!isValid ? "invalid" : " "}`}>
+        <label>Course Goal</label>
+        <input type="text" onChange={goalInputChangeHandler} />
+      </div>
+      <Button type="submit">Add Goal</Button>
+    </form>
+  );
+};
+
+export default CourseInput;
+
+/**
+ * The CSS STYLE
+ * 
+ * 
+ * .form-control {
+  margin: 0.5rem 0;
+}
+
+.form-control label {
+  font-weight: bold;
+  display: block;
+  margin-bottom: 0.5rem;
+}
+
+.form-control input {
+  display: block;
+  width: 100%;
+  border: 1px solid #ccc;
+  font: inherit;
+  line-height: 1.5rem;
+  padding: 0 0.25rem;
+}
+
+.form-control input:focus {
+  outline: none;
+  background: #fad0ec;
+  border-color: #8b005d;
+}
+
+We are going to add the invalid class dynamically 
+
+.form-control.invalid input {
+    border-color: red;
+    background: #ffd7d7;
+  }
+  
+  .form-control.form-control.invalid label {
+    color: red;
+  }
+  
+ */
