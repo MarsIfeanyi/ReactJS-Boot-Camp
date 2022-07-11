83322ae0be9c0497c19a01f39087fd49ea6d9057
Mars Ifeanyi <Marschalice@gmail.com>
2022-07-11 12:00:31 +0100

    Update file

--- react-complete-guide/src/components/Expenses/Expenses.js
+++ react-complete-guide/src/components/Expenses/Expenses.js
@@ -5,8 +5,9 @@ import "./Expenses.css";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
-function Expenses(props) {
+const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020"); //Here 2020 is set as the initial state value
 
   const filterChangeHandler = (selectedYear) => {
@@ -28,11 +29,13 @@ function Expenses(props) {
           onChangeFilter={filterChangeHandler}
         ></ExpensesFilter>
 
+        <ExpensesChart expenses={filteredExpenses}></ExpensesChart>
+
         <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
   );
-}
+};
 
 export default Expenses;
 
