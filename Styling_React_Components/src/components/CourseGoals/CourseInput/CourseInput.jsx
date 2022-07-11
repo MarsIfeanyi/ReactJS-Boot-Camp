e85d00d6cc67b036379e8956436ea367c35fcc98
Mars Ifeanyi <Marschalice@gmail.com>
2022-07-11 14:19:26 +0100

    Add CourseInput component

---
+++ Styling_React_Components/src/components/CourseGoals/CourseInput/CourseInput.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+
+import Button from "../../UI/Button/Button";
+import "./CourseInput.css";
+
+const CourseInput = (props) => {
+  const [enteredValue, setEnteredValue] = useState("");
+
+  const [isValid, setIsValid] = useState(true);
+
+  const goalInputChangeHandler = (event) => {
+    if (event.target.value.trim().length > 0) {
+      setIsValid(true);
+    }
+
+    setEnteredValue(event.target.value);
+  };
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+
+    //trim is a built method that we use to remove white spaces
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
+    props.onAddGoal(enteredValue);
+  };
+
+  return (
+    <form onSubmit={formSubmitHandler}>
+      <div className="form-control">
+        {/* Setting Dynamic inline styles...Hint we pass in an object{} into the style prop. 
+        
+        style={{ color: !isValid ? "red" : "black"*/}
+        <label style={{ color: !isValid ? "red" : "black" }}>Course Goal</label>
+        <input
+          style={{
+            borderColor: !isValid ? "red" : "#ccc",
+            background: !isValid ? "salmon" : "transparent",
+          }}
+          type="text"
+          onChange={goalInputChangeHandler}
+        />
+      </div>
+      <Button type="submit">Add Goal</Button>
+    </form>
+  );
+};
+
+export default CourseInput;
+
+//Hint: Inline style has the highest pirority in css
