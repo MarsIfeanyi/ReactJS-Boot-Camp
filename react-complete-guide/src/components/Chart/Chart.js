329b36f10e97423d25b5c8ff9f1f1b613cf327cd
Mars Ifeanyi <Marschalice@gmail.com>
2022-07-11 11:59:24 +0100

    Update file

--- react-complete-guide/src/components/Chart/Chart.js
+++ react-complete-guide/src/components/Chart/Chart.js
@@ -4,13 +4,18 @@ import ChartBar from "./ChartBar";
 import "./Chart.css";
 
 const Chart = (props) => {
+  // Here are transforming a datapoint object to just a number by using map
+  const dataPointValues = props.dataPoints.map((dataPoint) => dataPoint.value);
+  //Maximum value
+  const totalMaximum = Math.max(...dataPointValues);
+
   return (
     <div className="chart">
       {props.dataPoints.map((dataPoint) => (
         <ChartBar
           key={dataPoint.label}
           value={dataPoint.value}
-          maxValue={null}
+          maxValue={totalMaximum}
           label={dataPoint.label}
         ></ChartBar>
       ))}
