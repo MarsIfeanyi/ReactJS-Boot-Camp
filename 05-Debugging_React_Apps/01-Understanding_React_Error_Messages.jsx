bc05fd1db94bec3394f400948c6dd51382062cca
Mars Ifeanyi <Marschalice@gmail.com>
2022-07-12 08:41:13 +0100

    Debugging React Apps

---
+++ 05-Debugging_React_Apps/01-Understanding_React_Error_Messages.jsx
@@ -0,0 +1,4 @@
+/**
+ * For you to be a better developer, you need to understand and solve errors on your own.
+ * Hint:Always read the error message carefully to understand and identify where or line where the error is coming from
+ */
