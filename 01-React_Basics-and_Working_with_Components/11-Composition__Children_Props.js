18377ca22d488db27c73c03dcce4e72cc2810c65
Mars Ifeanyi <Marschalice@gmail.com>
2022-07-14 22:06:35 +0100

    Update documentation

--- 01-React_Basics-and_Working_with_Components/11-Composition__Children_Props.js
+++ 01-React_Basics-and_Working_with_Components/11-Composition__Children_Props.js
@@ -5,11 +5,10 @@
  *
  * Hint: Components are the custom HTML elements where you combine HTML code(JSX code)  and styling
  *
- *
- * Hint: You can't just your custom components like that as wrappers for other components ie having content between the opening and closing tags of that component doesn't just work like that.
+ * Hint: You can't just use your custom components like that as wrappers for other components ie having content between the opening and closing tags of that component doesn't just work like that.
  *
  * Props children is a special props which is built into React and every components has it even if you didn't set it explicitly.
- * Hint: Children is a reserved name and the value of the children props will always be the content content between the opening and closing tags of your custom component
+ * Hint VIP: Children is a reserved name and the value of the children props will always be the content between the opening and closing tags of your custom component
  *
  * Hint: Whenever you combine components, you are using composition. Composition is very important when working with react and an important part of composition is the 'props.children' feature which allows you to also create wrapper components
  */
