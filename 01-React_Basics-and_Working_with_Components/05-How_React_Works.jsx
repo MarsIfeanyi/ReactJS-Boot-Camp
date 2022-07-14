7bd9ab89bfa8ee31793d880ea89cb95ac78fe2d3
Mars Ifeanyi <Marschalice@gmail.com>
2022-07-14 22:04:19 +0100

    Update documentation

--- 01-React_Basics-and_Working_with_Components/05-How_React_Works.jsx
+++ 01-React_Basics-and_Working_with_Components/05-How_React_Works.jsx
@@ -1,11 +1,6 @@
 /**
- * We build our own custom HTML elements with React.
+ * We build our own custom HTML elements (JSX Code) with React.
  *
  * Component is basically just a custom HTML element, which we create using the Declarative approach ie with react we defined the desired target state and then react is responsible for generating and running the actual DOM instructions which updates whats visible on the screen
- *
- *
- *
- *
- *
  *
  */
