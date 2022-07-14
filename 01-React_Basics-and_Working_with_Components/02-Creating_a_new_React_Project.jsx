22244170b42bcc6b44c7b54ac38f492a13ce6f3f
Mars Ifeanyi <Marschalice@gmail.com>
2022-07-14 22:03:06 +0100

    Update documentation

--- 01-React_Basics-and_Working_with_Components/02-Creating_a_new_React_Project.jsx
+++ 01-React_Basics-and_Working_with_Components/02-Creating_a_new_React_Project.jsx
@@ -2,6 +2,6 @@
  * https://reactjs.org/docs/create-a-new-react-app.html
  *
  * The package.json file holds all the dependencies of the project
- * The Src folder holds the actual source code you will be working on
+ * The Source (Src) folder holds the actual source code you will be working on
  *
  */
