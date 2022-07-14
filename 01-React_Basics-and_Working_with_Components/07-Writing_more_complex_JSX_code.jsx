6f6d36d6ee1b46a41e159c78da115f2d9daa80e2
Mars Ifeanyi <Marschalice@gmail.com>
2022-07-14 22:04:59 +0100

    Update Documentation

--- 01-React_Basics-and_Working_with_Components/07-Writing_more_complex_JSX_code.jsx
+++ 01-React_Basics-and_Working_with_Components/07-Writing_more_complex_JSX_code.jsx
@@ -1,3 +1,3 @@
 /**
- * Hint: In react component there is one important rule,regarding the HTML, JSX code that is returned inside of a component, You MUST have only one root element per return statement or per JSX code snippet
+ * Hint: In react component there is one important rule,regarding the HTML (JSX) code that is returned inside of a component, You MUST have only one root element per return statement or per JSX code snippet
  */
