f53ac3b104f9df226ccef437abfc746b7f0b23cb
Mars Ifeanyi <Marschalice@gmail.com>
2022-07-14 22:05:38 +0100

    Update Documentation

--- 01-React_Basics-and_Working_with_Components/09-Outputting_Dynamic_Data_and_working_with_expression_in_JSX.js
+++ 01-React_Basics-and_Working_with_Components/09-Outputting_Dynamic_Data_and_working_with_expression_in_JSX.js
@@ -1,3 +1,4 @@
 /**
- * You can replace hardcoded data by using the {} and you can also run JS expressions inside the curly braces
+ * You can replace hardcoded data by using the curly bracket {} and you can also run JS expressions inside the curly braces.
+ * React treats everything inside the curly bracket as JS expression
  */
