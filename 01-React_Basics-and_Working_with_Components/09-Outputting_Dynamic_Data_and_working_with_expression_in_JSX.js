45f0fe4236925b9227a3568fb7569e5b6e413563
Mars Ifeanyi <Marschalice@gmail.com>
2022-07-15 02:57:06 +0100

    Update documentation

--- 01-React_Basics-and_Working_with_Components/09-Outputting_Dynamic_Data_and_working_with_expression_in_JSX.js
+++ 01-React_Basics-and_Working_with_Components/09-Outputting_Dynamic_Data_and_working_with_expression_in_JSX.js
@@ -1,4 +1,6 @@
 /**
  * You can replace hardcoded data by using the curly bracket {} and you can also run JS expressions inside the curly braces.
- * React treats everything inside the curly bracket as JS expression
+ * React treats everything inside the curly bracket as JS expression.
+ *
+ * Hint: You declear the variables outside the component's return statement and then you will use curly braces {} and point the variables decleared outside the return statement ie just put in the name of the decleared variable inside the {}
  */
