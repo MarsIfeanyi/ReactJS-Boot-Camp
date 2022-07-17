2c8052e36cffbb42b8e097824d0f7c160257c587
Mars Ifeanyi <Marschalice@gmail.com>
2022-07-17 21:58:24 +0100

    Update file

--- 02-React_State_and_Working_witth_Events/08-Handling_Form_Submission.js
+++ 02-React_State_and_Working_witth_Events/08-Handling_Form_Submission.js
@@ -25,7 +25,7 @@ const ExpenseForm = () => {
     //   enteredTitle: event.target.value,
     // });
 
-    //Altenative way for updating state that depends on the previous state
+    //Alternative way for updating state that depends on the previous state
     // setUserInput((prevState) => {
     //   return { ...prevState, enteredTitle: event.target.value };
     // });
