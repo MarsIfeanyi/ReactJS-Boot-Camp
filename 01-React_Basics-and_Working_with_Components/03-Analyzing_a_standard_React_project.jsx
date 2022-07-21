b069bbe3100d57066c739536fe11920e3f0a5c53
Mars Ifeanyi <Marschalice@gmail.com>
2022-07-21 23:27:25 +0100

    Update file

--- 01-React_Basics-and_Working_with_Components/03-Analyzing_a_standard_React_project.jsx
+++ 01-React_Basics-and_Working_with_Components/03-Analyzing_a_standard_React_project.jsx
@@ -7,4 +7,5 @@
  *
  * Hint: The convention for naming files in react is every new words starts with capital letter
  *
+ * The Index.html file in the public folder, is the single HTML file, which in the end is loaded by the browser. Its the only HTML file that is being used by the overall React application (Single page application) and this single HTML file is the entry point, the place where the React-driven user interface should be rendered into. it contains a single div with an id of 'root' where we inject our React-driven user interface.
  */
