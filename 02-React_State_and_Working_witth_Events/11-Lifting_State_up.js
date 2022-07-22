14cadeb86b628763c8d59906b2421ebd01cd2073
Mars Ifeanyi <Marschalice@gmail.com>
2022-07-22 23:02:45 +0100

    Update file

--- 02-React_State_and_Working_witth_Events/11-Lifting_State_up.js
+++ 02-React_State_and_Working_witth_Events/11-Lifting_State_up.js
@@ -3,5 +3,5 @@
  *
  * We don't have direct connection between two sibling components, instead we can only communicate from parent to child and from child to parent
  *
- * Lifting the State up is moving data from the child component to some parent component to either use it there or to then pass it down to some other child component
+ * Lifting the State up is moving data from the child component to some parent component (Bottom-up) to either use it there or to then pass it down to some other child component
  */
