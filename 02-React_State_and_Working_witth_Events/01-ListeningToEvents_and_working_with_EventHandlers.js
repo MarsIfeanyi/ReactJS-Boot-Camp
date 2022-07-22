065799ff820e1b5aa9c7cd33df234a14263190af
Mars Ifeanyi <Marschalice@gmail.com>
2022-07-22 15:29:32 +0100

    Update documentation

--- 02-React_State_and_Working_witth_Events/01-ListeningToEvents_and_working_with_EventHandlers.js
+++ 02-React_State_and_Working_witth_Events/01-ListeningToEvents_and_working_with_EventHandlers.js
@@ -3,4 +3,6 @@
  *
  * Hint: React expose all the default event which starts with 'on' as props.
  * The value assigned to the special prop is a function which contains code that should be executed when certain operation happens and we just point at the function inside {} ie you don't add () after the function name.
+ *
+ * Hint: You can choose the convention to name your function with the 'Handler' keyword at the end of the name, to show that they are triggered upon an event eg. clickHandler. This makes it clear to understand that this is a function which is not called by us, somewhere else in our code, but that it is a function we attach to an event listener and its called by React when that event occurs
  */
